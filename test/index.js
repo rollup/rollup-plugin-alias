9cc8acac92489563dc60e7ed552b8b3c6378cb29
Johannes Stein <johannesstein@freeze-dev.com>
2016-02-29 14:58:06 +0100

    Make tests more explicit

--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 import test from 'ava';
 
-import path from 'path';
-
 import { rollup } from 'rollup';
 import alias from '../dist/rollup-plugin-alias';
 
@@ -15,11 +13,11 @@ test(t =>
       './numberFolder': './folder',
     })],
   }).then(stats => {
-    t.is(path.basename(stats.modules[0].id), 'nonAliased.js');
-    t.is(path.basename(stats.modules[1].id), 'aliasMe.js');
-    t.is(path.basename(stats.modules[2].id), 'localAliasMe.js');
-    t.is(path.basename(stats.modules[3].id), 'anotherNumber.js');
-    t.is(path.basename(stats.modules[4].id), 'index.js');
+    t.is(stats.modules[0].id.endsWith('/files/nonAliased.js'), true);
+    t.is(stats.modules[1].id.endsWith('/files/aliasMe.js'), true);
+    t.is(stats.modules[2].id.endsWith('/files/localAliasMe.js'), true);
+    t.is(stats.modules[3].id.endsWith('/files/folder/anotherNumber.js'), true);
+    t.is(stats.modules[4].id.endsWith('/files/index.js'), true);
     t.is(stats.modules.length, 5);
   })
 );
