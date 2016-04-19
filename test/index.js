f40c9db98d57ace06d2b260da72d9bb4a68f06f4
Daniel Koch <daniel@suitsoft.eu>
2016-04-19 08:51:00 +0200

    Add a few more tests

--- test/index.js
+++ test/index.js
@@ -3,6 +3,51 @@ import test from 'ava';
 import { rollup } from 'rollup';
 import alias from '../dist/rollup-plugin-alias';
 
+test(t => {
+  t.is(typeof alias, 'function');
+});
+
+test(t => {
+  const result = alias();
+  t.is(typeof result, 'object');
+  t.is(typeof result.resolveId, 'function');
+});
+
+test(t => {
+  const result = alias({});
+  t.is(typeof result, 'object');
+  t.is(typeof result.resolveId, 'function');
+});
+
+// Simple aliasing
+test(t => {
+  const result = alias({
+    foo: 'bar',
+    pony: 'paradise',
+  });
+
+  const resolved = result.resolveId('foo', '/src/importer.js');
+  const resolved2 = result.resolveId('pony', '/src/importer.js');
+
+  t.is(resolved, 'bar');
+  t.is(resolved2, 'paradise');
+});
+
+// Local aliasing
+test(t => {
+  const result = alias({
+    foo: './bar',
+    pony: './par/a/di/se',
+  });
+
+  const resolved = result.resolveId('foo', '/src/importer.js');
+  const resolved2 = result.resolveId('pony', '/src/highly/nested/importer.js');
+
+  t.is(resolved, '/src/bar.js');
+  t.is(resolved2, '/src/highly/nested/par/a/di/se.js');
+});
+
+// Tests in Rollup
 test(t =>
   rollup({
     entry: './files/index.js',
