5d57d74d98c21ab528e44002560d57255afae1ab
Zachary Golba <zachary.golba@postlight.com>
2016-09-11 16:15:55 -0400

    fix: isFilePath not working on windows

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 const noop = () => null;
 const startsWith = (needle, haystack) => ! haystack.indexOf(needle);
 const endsWith = (needle, haystack) => haystack.slice(-needle.length) === needle;
-const isFilePath = id => /^\.?\//.test(id);
+const isFilePath = id => new RegExp(`^\.?\\${path.sep}`).test(id);
 const exists = uri => {
   try {
     return fs.statSync(uri).isFile();
